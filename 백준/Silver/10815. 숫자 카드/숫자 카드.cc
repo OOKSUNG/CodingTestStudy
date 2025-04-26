085eb5916fae29c7d8b67bb4627baa53a09a56f9
Kwon Seong Uk <115702895+OOKSUNG@users.noreply.github.com>
2025-04-26 23:22:12 +0900

    [Silver V] Title: 숫자 카드, Time: 204 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+int fn(const int &x, const long long *a, const int &n) {
+    int first = 0;
+    int max = n - 1;
+    int mid = 0; 
+    while (first < max) {
+        mid = (first + max) / 2;
+        if (x == a[mid]) return 1;
+        else if (x > a[mid]) first = mid + 1;
+        else if (x < a[mid]) max = mid - 1;
+    }
+    if (a[first] == x) return 1;
+    else return 0;
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+
+    int N, M;
+    cin >> N;
+    long long * A = new long long[N];
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    sort(A, A + N);
+
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int x;
+        int sum = 0;
+        cin >> x;
+        
+        cout << fn(x, A, N) << " ";
+    }
+}
