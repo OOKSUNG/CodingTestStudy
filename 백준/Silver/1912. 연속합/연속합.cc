6995d0c23448dc64b4b4b1a7ffc962a44da8dc0c
Kwon Seong Uk <115702895+OOKSUNG@users.noreply.github.com>
2025-05-14 15:54:04 +0900

    [Silver II] Title: 연속합, Time: 20 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.cc
+++ 백준/Silver/1912. 연속합/연속합.cc
@@ -11,12 +11,12 @@ int main()
 	int N;
 	cin >> N;
 	for (int i = 0; i < N; i++) cin >> Arr[i];
-	
+
 	DP[0] = Arr[0];
 	int answer = DP[0];
 	for (int i = 1; i < N; i++) {
-		DP[i] = max(Arr[i],Arr[i] + DP[i - 1]);
-		answer = max(answer, max(DP[i - 1], DP[i]));
+		DP[i] = max(Arr[i], Arr[i] + DP[i - 1]);
+		answer = max(answer, DP[i]);
 	}
 	cout << answer;
 }
