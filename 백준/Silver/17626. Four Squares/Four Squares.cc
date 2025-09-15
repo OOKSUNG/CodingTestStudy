ee4837c32a429cee31ef0e5690dd2ea11e57cdbb
Kwon Seong Uk <115702895+OOKSUNG@users.noreply.github.com>
2025-09-15 19:58:12 +0900

    [Silver III] Title: Four Squares, Time: 12 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> dp(n + 1, 1e9); // 큰 값으로 초기화
+    dp[0] = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j * j <= i; j++) {
+            dp[i] = min(dp[i], dp[i - j * j] + 1);
+        }
+    }
+
+    cout << dp[n] << "\n";
+    return 0;
+}
