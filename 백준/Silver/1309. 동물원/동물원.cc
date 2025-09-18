6e39ae36eb71b2fb7ae528a54b4c21710e0c8676
Kwon Seong Uk <115702895+OOKSUNG@users.noreply.github.com>
2025-09-18 15:17:57 +0900

    [Silver I] Title: 동물원, Time: 8 ms, Memory: 7400 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    int N;
+    cin >> N;
+
+    vector<vector<int>> dp(N, vector<int>(3,0));
+    dp[0][0] = 1; dp[0][1] = 1; dp[0][2] = 1;
+
+    for (int i = 1; i < N; i++) {
+        dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][0]) % 9901;
+        dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+        dp[i][2] = (dp[i - 1][1] + dp[i - 1][0]) % 9901;
+    }
+    int result = (dp[N - 1][0] + dp[N - 1][1] + dp[N - 1][2]) % 9901;
+    cout << result;
+}
