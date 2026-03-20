6514a1bc5973aa6b9e718cc23b879ca7c025a2b7
Kwon Seong Uk <115702895+OOKSUNG@users.noreply.github.com>
2026-03-21 00:57:30 +0900

    [Silver I] Title: 경로 찾기, Time: 4 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.cc
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.cc
@@ -5,43 +5,31 @@
 
 using namespace std;
 int N;
+int arr[100][100];
 int ans[100][100];
-vector<vector<int>> v;
 
-void bfs(){
-    stack<int> st;
+int main() {
+    cin >> N;
+
     for(int i = 0; i < N; i++){
-        st.push(i);
-        while(!st.empty()){
-            int cur = st.top();
-            st.pop();
-            
-            for(int j = 0; j < (int)v[cur].size(); j++){
-                if(ans[i][v[cur][j]] != 1) {
-                    st.push(v[cur][j]);
-                    ans[i][v[cur][j]] = 1;
+        for(int j = 0; j < N; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    for(int m = 0; m < N; m++){
+        for(int s = 0; s < N; s++){
+            for(int e = 0; e < N; e++){
+                if(arr[s][m] && arr[m][e]){
+                    arr[s][e] = 1;
                 }
             }
         }
     }
-}
-
-int main() {
-    cin >> N;
-    v.resize(N);
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            int x;
-            cin >> x;
-            if(x) v[i].push_back(j);
-        }
-    }
-    
-    bfs();
     
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
-            cout << ans[i][j] << ' ';
+            cout << arr[i][j] << ' ';
         }
         cout << '\n';
     }
