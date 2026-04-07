f7eb3601b8bd95ebd6225934fd8367db765e55fe
Kwon Seong Uk <115702895+OOKSUNG@users.noreply.github.com>
2026-04-07 09:54:45 +0900

    [Bronze IV] Title: 주사위, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    for(int i = 1; i <= T; i++){
+        int x, y;
+        cin >> x >> y;
+        cout << "Case " << i << ": " << x+y << '\n';
+        
+    }
+    
+    return 0;
+}
