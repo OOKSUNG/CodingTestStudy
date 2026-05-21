ff5cc14db86f2f2035657c8464dd42fa8604adbd
Kwon Seong Uk <115702895+OOKSUNG@users.noreply.github.com>
2026-05-21 12:55:36 +0900

    Time: 0 ms (100%), Space: 12.4 MB (98.38%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     vector<int> runningSum(vector<int>& nums) {
-        vector<int> ans(nums.size(), 0);
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j <= i; j++){
-                ans[i] += nums[j];
-            }
+        for(int i = 1; i < nums.size(); i++){
+            nums[i] += nums[i-1];
         }
-        return ans;
+        return nums;
     }
 };
